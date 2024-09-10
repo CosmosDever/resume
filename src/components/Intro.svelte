099ca7dfbf7ddaa76132ac0364dc3a8453b11186
CosmosDever <aprommhgot@gmail.com>
2024-09-11 00:38:43 +0700

    .

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -32,9 +32,7 @@
 		<p>
 			<a href={`https://${website}`} target="_blank" rel="noreferrer">{website}</a>
 		</p>
-		<p>
-			<a href={`https://linkedin.com/in/${linkedin}`} target="_blank" rel="noreferrer">Linkedin</a>
-		</p>
+		<p />
 	</div>
 </div>
 
